f143f73cb1c9f26246a9e7bdfef6f0fcb92ea767
joywhy <leh9111@naver.com>
2023-04-05 01:06:30 +0900

    useeffect 조건부 effect 발생

--- weathercity/src/App.js
+++ weathercity/src/App.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useEffect,useState } from "react";
 import './App.css';
+import{getWeather} from "./weatherapi";
 import Search from "./Search";
 import Showapi from "./Showapi";
 
 function App() {
+const [condition,setcondition]=useState("");
+
+function search(city){
+ 
+  if(condition!==city){
+     setcondition(city);
+    
+   }
+   console.log(condition);  //왜 처음 클릭 햇을때 빈문자 그다음 서울이 나올까 
+}
+useEffect(()=>{
+  console.log("작동"); //처음에 왜 작동이 두번이 뜨나
+  getWeather(condition);
+
+},[condition]);
+
   return (
     <div className="App">
-      <Search/>
+      <Search onSearch={search}/>
       <Showapi/>
     </div>
   );
--- weathercity/src/Search.js
+++ weathercity/src/Search.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 
-function Search(){
+function Search({onSearch}){
+const [textcity,setcity]=useState("");
 
+function handlecity(e){
+setcity(e.target.value);
+}
+function handleSearchBtn(){
+    const city = textcity===""?null:textcity;
+
+    onSearch(city);
+
+    // if(city!==""){  //그냥 city 문자열 안넣고 state 로 바로 넣으면 안되나?
+    //     onSearch(city);
+    // }
+
+
+}
 return(
     <div>
-        <input></input>
-       <button>버튼</button> 
+        <input type ="text" value={textcity} onChange ={handlecity}></input>
+       <button onClick={handleSearchBtn}>버튼</button> 
     </div>
 );
 
